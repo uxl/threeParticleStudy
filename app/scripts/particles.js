b316ef72dc2e39c3ea22329d2e7a3655179cab04
uxl <brian@ulx.io>
2015-12-01 12:40:10 -0500

    add map of world add some sliders to dat.gui for future enhancements

--- app/scripts/particles.js
+++ app/scripts/particles.js
@@ -21,16 +21,24 @@ var PARTICLES = (function() {
         windowHalfY = window.innerHeight / 2,
         resetMe = false,
         gui = null,
+        context = null,
+        canvas = null,
+        geometry = null,
+        testbox = null,
 
         //particle init values
         settings = {
-            speed:100000,
-            spread:1000,
-            number:280,
-            maxSize:32,
-            minSize:16,
-            symbol:0,
-            focus:0
+            speed: 100000,
+            spread: 1000,
+            number: 280,
+            maxSize: 32,
+            minSize: 16,
+            symbol: 0,
+            focus: 50,
+            hue: 280,
+            saturation: 40,
+            width: 1920,
+            height: 1080
         },
 
         init = function() {
@@ -69,6 +77,48 @@ var PARTICLES = (function() {
             animate();
             addGui();
 
+            //geometry
+            geometry = new THREE.PlaneGeometry(1920, 1080, 200);
+
+
+            var material = new THREE.MeshBasicMaterial({
+                color: Math.random() * 0xffffff.
+                // opacity: 0.5 
+            });
+
+            var mesh = new THREE.Mesh(geometry, material);
+            scene.add(mesh);
+
+            var egh = new THREE.EdgesHelper(mesh, 0xffffff);
+            egh.material.linewidth = 2;
+            scene.add(egh);
+
+            // var textureLoader = new THREE.TextureLoader();
+            // textureLoader.load('images/night.jpg', function(){
+            //     var fillMesh = new THREE.Mesh(geometry, map);
+            //     scene.add( fillMesh );
+            // });
+
+            var loader = new THREE.TextureLoader();
+            loader.load('images/night.jpg', function(texture) {
+
+                //var geometry = new THREE.SphereGeometry( 200, 20, 20 );
+
+                var material = new THREE.MeshBasicMaterial({
+                    map: texture,
+                                    side: THREE.FrontSide,
+
+                    overdraw: true
+                });
+                var mesh = new THREE.Mesh(geometry, material);
+                scene.add(mesh);
+                //raycaster = new THREE.Raycaster();
+            });
+
+            // var texture = texture = THREE.TextureLoader( 'images/night.jpg' );
+            //var imageMat = new THREE.MeshBasicMaterial( {color:0xffffff, map: texture } );
+
+
             //console.log('1 -- ' + scene);
             //console.log('2 -- ' + scene.children);
             //console.log('3 -- ' + scene.getObjectByName('part1', true))
@@ -102,8 +152,9 @@ var PARTICLES = (function() {
             }
             render();
         },
-        bokeh = function(x, y, r, symbol, s, rr, ro) {
-            context.shadowBlur = r / settings.minsize * (Math.random() * 9.9 + 0.1);
+        bokeh = function(x, y, scale, symbol, verticies, randomRotation, rotation) {
+            console.log("bokeh: " + scale);
+            context.shadowBlur = scale / settings.minsize * (Math.random() * 9.9 + 0.1);
 
             context.strokeStyle = "rgba(0, 0, 0, 0.5)";
             context.fillStyle = "rgba(0, 0, 0, 0.3)";
@@ -113,51 +164,53 @@ var PARTICLES = (function() {
             context.beginPath();
             switch (parseInt(symbol)) {
                 case 0:
-                    context.arc(0, 0, r, 0, 2 * Math.PI);
+                    context.arc(0, 0, scale, 0, 2 * Math.PI);
                     break;
                 case 1:
-                    polygon(r, s, rr, ro);
+                    polygon(scale, verticies, randomRotation, rotation);
                     break;
                 case 2:
-                    nstar(r, s, rr, ro);
+                    nstar(scale, verticies, randomRotation, rotation);
                     break;
                 case 3:
-                    heart(r, rr, ro);
+                    heart(scale, randomRotation, rotation);
                     break;
             }
             context.fill();
             context.stroke();
             context.restore();
+
+
         },
-        polygon = function(r, s, rr, ro) {
-            a = 2 * Math.PI / s;
+        polygon = function(scale, verticies, randomRotation, rotation) {
+            a = 2 * Math.PI / verticies;
 
-            context.rotate(rr * (1 - 2 * Math.random()) * Math.PI + a / 2 + ro);
+            context.rotate(randomRotation * (1 - 2 * Math.random()) * Math.PI + a / 2 + rotation);
 
-            context.moveTo(0, r);
-            for (var i = 0; i < s; i++) {
+            context.moveTo(0, scale);
+            for (var i = 0; i < verticies; i++) {
                 context.rotate(a);
-                context.lineTo(0, r);
+                context.lineTo(0, scale);
             }
         },
-        nstar = function(r, s, rr, ro) {
-            a = 2 * Math.PI / s;
+        nstar = function(scale, verticies, randomRotation, rotation) {
+            a = 2 * Math.PI / verticies;
 
-            context.rotate(rr * (1 - 2 * Math.random()) * Math.PI + a / 2 + ro);
+            context.rotate(randomRotation * (1 - 2 * Math.random()) * Math.PI + a / 2 + rotation);
 
             context.moveTo(0, r);
-            for (var i = 0; i < 2 * s; i++) {
+            for (var i = 0; i < 2 * verticies; i++) {
                 context.rotate(a / 2);
-                context.lineTo(0, r - (i % 2 == 0) * 2 * r / 3);
+                context.lineTo(0, scale - (i % 2 == 0) * 2 * scale / 3);
             }
         },
-        heart = function(r, rr, ro) {
-            context.rotate(rr * (1 - 2 * Math.random()) * Math.PI + ro);
+        heart = function(scale, randomRotation, rotation) {
+            context.rotate(randomRotation * (1 - 2 * Math.random()) * Math.PI + rotation);
 
-            context.moveTo(0, 3 * r / 2);
-            context.arc(-r / 2, 0, r / 2, 3 * Math.PI / 4, 0);
-            context.arc(r / 2, 0, r / 2, Math.PI, Math.PI / 4);
-            context.lineTo(0, 3 * r / 2);
+            context.moveTo(0, 3 * scale / 2);
+            context.arc(-scale / 2, 0, scale / 2, 3 * Math.PI / 4, 0);
+            context.arc(scale / 2, 0, scale / 2, Math.PI, Math.PI / 4);
+            context.lineTo(0, 3 * scale / 2);
         },
         onWindowResize = function() {
 
@@ -178,11 +231,6 @@ var PARTICLES = (function() {
 
             var context = canvas.getContext('2d');
 
-            //   canvas.width = settings.width;
-            //   canvas.height = 3 * settings.height;
-            //   context = canvas.getContext("2d");
-
-
             context.fillStyle = "000";
             context.globalCompositeOperation = 'source-over';
             context.fillRect(0, 0, canvas.width, canvas.height);
@@ -190,18 +238,6 @@ var PARTICLES = (function() {
             context.lineWidth = 3;
             context.shadowColor = "hsl(0, 0%, 50%)";
             context.shadowOffsetY = -settings.height;
-
-            //   for (var i = 0; i < settings.number; i++) {
-            //     var x = Math.floor(1920 * Math.random()),
-            //         y = Math.floor(1080 * Math.random()),
-            //         r = maxsize - (settings.maxsize - settings.minsize) * Math.random();
-
-            //       var randomrot = 0;
-            //       var rotation = 0;
-            //       var vertices = 2;
-
-            //     bokeh(x, y, r, symbol, vertices, randomrot, Math.PI * rotation / 180);
-            //   }
 
             var gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2);
             gradient.addColorStop(0, 'rgba(255,255,100,0.5)');
@@ -325,7 +361,9 @@ var PARTICLES = (function() {
                 min_size: settings.minSize,
                 symbol: settings.symbol,
                 focus: settings.focus,
-                reset: resetScene
+                reset: resetScene,
+                hue: settings.hue,
+                saturation: settings.saturation
             };
             gui.add(params, "symbol", {
                 circle: 0,
@@ -385,48 +423,78 @@ var PARTICLES = (function() {
                 render();
                 animate();
             });
-            gui.add(params, "focus" ).min(0).max(1000).step(100).onFinishChange(function(){
+            gui.add(params, "focus").min(0).max(1000).step(100).onFinishChange(function() {
                 //refresh shaderfocus
                 alert('future feature');
             });
+            gui.add(params, "hue", 0, 360).onFinishChange(function() {
+                alert('future feature');
+            });
+            gui.add(params, "saturation", 0, 100).onFinishChange(function() {
+                alert('future feature');
+            });
+
             gui.add(params, "reset");
-
-
-            //gui.add( effectController, "threshold", 0, 1, 0.001 ).onChange( matChanger );
-
-            // gui.add( effectController, "enabled" ).onChange( matChanger );
-            // gui.add( effectController, "jsDepthCalculation" ).onChange( matChanger );
-            // gui.add( effectController, "focalDepth", 0.0, 200.0 ).listen().onChange( matChanger );
-
-            // gui.add( effectController, "fstop", 0.1, 22, 0.001 ).onChange( matChanger );
-            // gui.add( effectController, "maxblur", 0.0, 5.0, 0.025 ).onChange( matChanger );
-
-            // gui.add( effectController, "showFocus" ).onChange( matChanger );
-            // gui.add( effectController, "manualdof" ).onChange( matChanger );
-            // gui.add( effectController, "vignetting" ).onChange( matChanger );
-
-            // gui.add( effectController, "depthblur" ).onChange( matChanger );
-
-            // gui.add( effectController, "threshold", 0, 1, 0.001 ).onChange( matChanger );
-            // gui.add( effectController, "gain", 0, 100, 0.001 ).onChange( matChanger );
-            // gui.add( effectController, "bias", 0,3, 0.001 ).onChange( matChanger );
-            // gui.add( effectController, "fringe", 0, 5, 0.001 ).onChange( matChanger );
-
-            // gui.add( effectController, "focalLength", 16, 80, 0.001 ).onChange( matChanger )
-
-            // gui.add( effectController, "noise" ).onChange( matChanger );
-
-            // gui.add( effectController, "dithering", 0, 0.001, 0.0001 ).onChange( matChanger );
-
-            // gui.add( effectController, "pentagon" ).onChange( matChanger );
-
-            // gui.add( shaderSettings, "rings", 1, 8).step(1).onChange( shaderUpdate );
-            // gui.add( shaderSettings, "samples", 1, 13).step(1).onChange( shaderUpdate );
-
-            // matChanger();
 
             window.addEventListener('resize', onWindowResize, false);
 
+
+        },
+        // RGB and HSL functions from
+        // https://gist.github.com/mjijackson/5311256
+        RTH = function(r, g, b) {
+            r /= 255, g /= 255, b /= 255;
+            var max = Math.max(r, g, b),
+                min = Math.min(r, g, b);
+            var h, s, l = (max + min) / 2;
+            if (max == min) {
+                h = s = 0;
+            } else {
+                var d = max - min;
+                s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+                switch (max) {
+                    case r:
+                        h = (g - b) / d + (g < b ? 6 : 0);
+                        break;
+                    case g:
+                        h = (b - r) / d + 2;
+                        break;
+                    case b:
+                        h = (r - g) / d + 4;
+                        break;
+                }
+                h /= 6;
+            }
+            return {
+                h: h,
+                s: s,
+                l: l
+            };
+        },
+        HTR = function(h, s, l) {
+            var r, g, b;
+            if (s == 0) {
+                r = g = b = l;
+            } else {
+                function hue2rgb(p, q, t) {
+                    if (t < 0) t += 1;
+                    if (t > 1) t -= 1;
+                    if (t < 1 / 6) return p + (q - p) * 6 * t;
+                    if (t < 1 / 2) return q;
+                    if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+                    return p;
+                }
+                var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+                var p = 2 * l - q;
+                r = hue2rgb(p, q, h + 1 / 3);
+                g = hue2rgb(p, q, h);
+                b = hue2rgb(p, q, h - 1 / 3);
+            }
+            return {
+                r: r * 255,
+                g: g * 255,
+                b: b * 255
+            };
         };
     return {
         init: init,
